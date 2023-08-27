80847358e1ddab2a5a99288f21e875127cd14246
CherifiMi <cherifi154949@gmail.com>
2023-08-27 11:48:00 +0100

    .

--- app/src/main/java/com/example/pikstudio/ui/studio/components/BottomTools.kt
+++ app/src/main/java/com/example/pikstudio/ui/studio/components/BottomTools.kt
@@ -2,49 +2,107 @@ package com.example.pikstudio.ui.studio.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.pikstudio.ui.theme.PiKStudioTheme
 
 
 @Composable
 fun BottomTools(modifier: Modifier = Modifier) {
     Column(modifier) {
-        Row(
+        FirstBottomRow(
             Modifier
                 .fillMaxWidth()
                 .height(88.dp)
                 .background(Color.Red)
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+                .padding(horizontal = 16.dp)
+        )
 
-        }
-        Row(
+        SecondBottomRow(
             Modifier
                 .fillMaxWidth()
                 .background(Color.Blue)
                 .height(64.dp)
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+                .padding(horizontal = 16.dp)
+        )
 
-        }
-        Row(
+        ThirdBottomRow(
             Modifier
                 .fillMaxWidth()
                 .background(Color.Green)
                 .height(112.dp)
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+                .padding(horizontal = 16.dp)
+        )
 
+    }
+}
+
+
+@Composable
+fun FirstBottomRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Row {
+            ToolBtn(Icons.Default.Face, 40.dp, 18.dp){}
+            Spacer(modifier = Modifier.size(8.dp))
+            ToolBtn(Icons.Default.Face, 40.dp, 18.dp){}
+            Spacer(modifier = Modifier.size(8.dp))
+            ToolBtn(Icons.Default.Face, 40.dp, 18.dp){}
         }
+
+        ToolBtnsBorder()
+
+        ToolBtn(Icons.Default.Face, 40.dp, 18.dp){}
+
+        ToolBtnsBorder()
+
+        ToolBtn(Icons.Default.Face, 40.dp, 18.dp){}
+
+        ToolBtnsBorder()
+
+        ToolBtn(Icons.Default.Face, 40.dp, 18.dp){}
+
     }
 
+}
+
+@Composable
+fun SecondBottomRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+    }
+}
+
+@Composable
+fun ThirdBottomRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BottomToolsPreview() {
+    PiKStudioTheme {
+        BottomTools(Modifier.fillMaxWidth())
+    }
 }
--- app/src/main/java/com/example/pikstudio/ui/studio/components/TopTools.kt
+++ app/src/main/java/com/example/pikstudio/ui/studio/components/TopTools.kt
@@ -3,6 +3,7 @@ package com.example.pikstudio.ui.studio.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -13,181 +14,173 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.pikstudio.ui.theme.PiKStudioTheme
 
 @Composable
 fun TopTools(modifier: Modifier = Modifier) {
     Column(modifier) {
-        Row(
+        FirstTopRow(
             Modifier
                 .fillMaxWidth()
                 .height(64.dp)
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier
-                    .width(30.dp)
-                    .height(38.dp)
-                    .background(Color.Blue)
-            )
-
-            Row(Modifier.wrapContentSize()) {
-                IconButton(modifier = Modifier
-                    .size(32.dp),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        modifier = Modifier.size(24.dp),
-                        imageVector = Icons.Default.Face, contentDescription = null)
-                }
-
-                Spacer(modifier = Modifier.size(8.dp))
-
-                IconButton(modifier = Modifier
-                    .size(32.dp),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(imageVector = Icons.Default.Face, contentDescription = null)
-                }
-
-                Spacer(modifier = Modifier.size(8.dp))
-
-                IconButton(modifier = Modifier
-                    .size(32.dp),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(imageVector = Icons.Default.Face, contentDescription = null)
-                }
-            }
-        }
-
-        Row(
+                .padding(horizontal = 16.dp)
+        )
+        SecondTopRow(
             Modifier
                 .fillMaxWidth()
                 .height(96.dp)
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Row {
-                IconButton(modifier = Modifier
-                    .size(36.dp).clip(RoundedCornerShape(8.dp))
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        modifier = Modifier.size(16.dp),
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
+                .padding(horizontal = 16.dp)
+        )
 
-                Spacer(modifier = Modifier.size(8.dp))
+    }
+}
 
-                IconButton(modifier = Modifier
-                    .size(36.dp)
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
+@Composable
+fun FirstTopRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .width(30.dp)
+                .height(38.dp)
+                .background(Color.Blue)
+        )
 
-                Spacer(modifier = Modifier.size(8.dp))
-
-                IconButton(modifier = Modifier
-                    .size(36.dp)
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
-
+        Row(Modifier.wrapContentSize()) {
+            IconButton(modifier = Modifier
+                .size(32.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Default.Face,
+                    contentDescription = null
+                )
             }
 
-            Box(
-                Modifier
-                    .padding(12.dp)
-                    .height(30.dp)
-                    .border(border = BorderStroke(3.dp, Color.Gray), RoundedCornerShape(100.dp))
-            )
+            Spacer(modifier = Modifier.size(8.dp))
 
-            Row {
-                IconButton(modifier = Modifier
-                    .size(36.dp)
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
-
-                Spacer(modifier = Modifier.size(8.dp))
-
-                IconButton(modifier = Modifier
-                    .size(36.dp)
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
+            IconButton(modifier = Modifier
+                .size(32.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Default.Face,
+                    contentDescription = null
+                )
             }
 
+            Spacer(modifier = Modifier.size(8.dp))
 
-            Box(
-                Modifier
-                    .padding(12.dp)
-                    .height(30.dp)
-                    .border(border = BorderStroke(3.dp, Color.Gray), RoundedCornerShape(100.dp))
-            )
-
-
-            Row {
-                IconButton(modifier = Modifier
-                    .size(36.dp)
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
-
-                Spacer(modifier = Modifier.size(16.dp))
-
-                IconButton(modifier = Modifier
-                    .size(36.dp)
-                    .background(Color.Gray),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Face,
-                        contentDescription = null,
-                        tint = Color.Blue
-                    )
-                }
+            IconButton(modifier = Modifier
+                .size(32.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Default.Face,
+                    contentDescription = null
+                )
             }
         }
     }
+}
 
+@Composable
+fun SecondTopRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row {
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+        }
+
+        ToolBtnsBorder()
+
+        Row {
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+        }
+
+        ToolBtnsBorder()
+
+        Row {
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            ToolBtn(Icons.Default.Face, 36.dp, 16.dp){}
+        }
+    }
+}
+
+
+@Composable
+fun ToolBtn(icon: ImageVector, btnSize: Dp, iconSize: Dp, click: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .size(btnSize)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Gray)
+            .clickable { click() },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(iconSize),
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color.Blue
+        )
+    }
+}
+
+@Composable
+fun ToolBtnsBorder() {
+    Box(
+        Modifier
+            .padding(12.dp)
+            .drawBehind {
+                drawRoundRect(
+                    color = Color.Gray,
+                    size = Size(height = 30.dp.toPx(), width = 3.dp.toPx()),
+                    cornerRadius = CornerRadius(100f, 100f)
+                )
+            }
+            .height(30.dp)
+            .border(border = BorderStroke(3.dp, Color.Gray), RoundedCornerShape(100.dp))
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun TopToolsPreview() {
+    PiKStudioTheme {
+        TopTools(Modifier.fillMaxWidth())
+    }
 }
