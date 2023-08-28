d5da477c3d8ca1c52eaf349f3e74358bb0a7f4c0
CherifiMi <cherifi154949@gmail.com>
2023-08-28 11:35:01 +0100

    .

--- app/src/main/java/com/example/pikstudio/ui/theme/Color.kt
+++ app/src/main/java/com/example/pikstudio/ui/theme/Color.kt
@@ -2,43 +2,150 @@ package com.example.pikstudio.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200 = Color(0xFF_BB86FC)
+val Purple500 = Color(0xFF_6200EE)
+val Purple700 = Color(0xFF_3700B3)
+val Teal200 = Color(0xFF_03DAC5)
 
-val Blu = Color(0xFF6195CA)
-val LightGry = Color(0xFFF3F3F3)
-val DarkGry = Color(0xFFBBBBBB)
-val Orange = Color(0xFFEFBA9B)
-val Red = Color(0xFFEC8282)
-val Purple = Color(0xFF826F8D)
+val Blu = Color(0xFF_6195CA)
+val LightGry = Color(0xFF_F3F3F3)
+val DarkGry = Color(0xFF_BBBBBB)
+val Orange = Color(0xFF_EFBA9B)
+val Red = Color(0xFF_EC8282)
+val Purple = Color(0xFF_826F8D)
 
 
-val Color1 = Color(0xFFD52127)
-val Color2 = Color(0xFFF36621)
-val Color3 = Color(0xFFF6851E)
-val Color4 = Color(0xFFFBB40F)
-val Color5 = Color(0xFFFCED23)
-val Color6 = Color(0xFF8CC640)
-val Color7 = Color(0xFF07B151)
-val Color8 = Color(0xFF2FBBB3)
-val Color9 = Color(0xFF2357BC)
-val Color10 = Color(0xFF4C489B)
-val Color11 = Color(0xFF733B97)
-val Color12 = Color(0xFFAF3A94)
+val Color1 = Color(0xFF_F9F871)
+val Color2 = Color(0xFF_FFC75F)
+val Color3 = Color(0xFF_FF9671)
+val Color4 = Color(0xFF_FF6F91)
+val Color5 = Color(0xFF_D65DB1)
+val Color6 = Color(0xFF_845EC2)
+val Color7 = Color(0xFF_2C73D2)
+val Color8 = Color(0xFF_008F7A)
+
+
+val Color1List = listOf(
+    Color(0xFF_FDFDD6),
+    Color(0xFF_FCFCC2),
+    Color(0xFF_FCFCAE),
+    Color(0xFF_FBFB9A),
+    Color(0xFF_FAFA85),
+    Color(0xFF_F9F871),
+    Color(0xFF_F7F73F),
+    Color(0xFF_F5F50E),
+    Color(0xFF_C7C708),
+    Color(0xFF_959506),
+    Color(0xFF_636304),
+    Color(0xFF_323202)
+)
+val Color2List = listOf(
+    Color(0xFF_FFEFD2),
+    Color(0xFF_FFE7BB),
+    Color(0xFF_FFDFA5),
+    Color(0xFF_FFD78E),
+    Color(0xFF_FFD077),
+    Color(0xFF_FFC75F),
+    Color(0xFF_FFB62F),
+    Color(0xFF_FBA300),
+    Color(0xFF_C98300),
+    Color(0xFF_976200),
+    Color(0xFF_654100),
+    Color(0xFF_322100)
+)
+val Color3List = listOf(
+    Color(0xFF_FFE1D6),
+    Color(0xFF_FFD2C2),
+    Color(0xFF_FFC3AD),
+    Color(0xFF_FFB499),
+    Color(0xFF_FFA585),
+    Color(0xFF_FF9671),
+    Color(0xFF_FF703C),
+    Color(0xFF_FF4907),
+    Color(0xFF_D23800),
+    Color(0xFF_9D2A00),
+    Color(0xFF_691C00),
+    Color(0xFF_340E00),
+)
+val Color4List = listOf(
+    Color(0xFF_FFD6E0),
+    Color(0xFF_FFC2D0),
+    Color(0xFF_FFADC0),
+    Color(0xFF_FF99B1),
+    Color(0xFF_FF85A1),
+    Color(0xFF_FF6F91),
+    Color(0xFF_FF3C69),
+    Color(0xFF_FF0741),
+    Color(0xFF_D20031),
+    Color(0xFF_9D0025),
+    Color(0xFF_690018),
+    Color(0xFF_34000C),
+)
+val Color5List = listOf(
+    Color(0xFF_F3D0E9),
+    Color(0xFF_EEB9DE),
+    Color(0xFF_E8A2D3),
+    Color(0xFF_E28AC8),
+    Color(0xFF_DC73BD),
+    Color(0xFF_D65DB1),
+    Color(0xFF_CD39A1),
+    Color(0xFF_AF2C88),
+    Color(0xFF_8C236C),
+    Color(0xFF_691A51),
+    Color(0xFF_461136),
+    Color(0xFF_23091B),
+)
+val Color6List = listOf(
+    Color(0xFF_DCD2EE),
+    Color(0xFF_CBBBE5),
+    Color(0xFF_BAA4DD),
+    Color(0xFF_A88DD4),
+    Color(0xFF_9777CB),
+    Color(0xFF_845EC2),
+    Color(0xFF_7045B5),
+    Color(0xFF_5D3997),
+    Color(0xFF_4A2E78),
+    Color(0xFF_38225A),
+    Color(0xFF_25173C),
+    Color(0xFF_130B1E),
+)
+val Color7List = listOf(
+    Color(0xFF_C3D7F2),
+    Color(0xFF_A5C4EC),
+    Color(0xFF_87B0E6),
+    Color(0xFF_699CDF),
+    Color(0xFF_4B88D9),
+    Color(0xFF_2C73D2),
+    Color(0xFF_2664B4),
+    Color(0xFF_205396),
+    Color(0xFF_1A4378),
+    Color(0xFF_13325A),
+    Color(0xFF_0D213C),
+    Color(0xFF_06111E),
+)
+val Color8List = listOf(
+    Color(0xFF_96FFEF),
+    Color(0xFF_62FFE7),
+    Color(0xFF_2DFFE0),
+    Color(0xFF_00F8D3),
+    Color(0xFF_00C3A6),
+    Color(0xFF_008F7A),
+    Color(0xFF_007A68),
+    Color(0xFF_006657),
+    Color(0xFF_005245),
+    Color(0xFF_003D34),
+    Color(0xFF_002923),
+    Color(0xFF_001411),
+)
+
 
 val ColorWheel = listOf(
     Color1,
     Color2,
-    //Color3,
+    Color3,
     Color4,
     Color5,
     Color6,
-    //Color7,
-    Color8,
-    //Color9,
-    //Color10,
-    Color11,
-    Color12
+    Color7,
+    Color8
 )
